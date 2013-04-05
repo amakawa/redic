62844a68d20d6e4fc9a3c945e1f2e4c7fb1b8f41
Cyril David <me@cyrildavid.com>
2013-04-05 12:34:12 -0700

    Add a default TIMEOUT of 10.
    Since hiredis defaults to no timeout, we're effectively
    left with a hanged ruby process in the event that it
    can't connect.
    
    We solve this by setting a high enough decent number that
    shouldn't generate false positives either.

--- lib/redic/connection.rb
+++ lib/redic/connection.rb
@@ -2,13 +2,15 @@ require "hiredis/connection"
 
 class Redic
   module Connection
+    TIMEOUT = 10_000_000
+
     def self.new(uri)
       connection = Hiredis::Connection.new
 
       if uri.scheme == "unix"
-        connection.connect_unix(uri.path)
+        connection.connect_unix(uri.path, TIMEOUT)
       else
-        connection.connect(uri.host, uri.port)
+        connection.connect(uri.host, uri.port, TIMEOUT)
       end
 
       connection
